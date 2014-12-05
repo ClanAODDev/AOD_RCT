0b654485b746c46c00d8cba9b5ec08b2238e53ee
Chris Deaton <dcdeaton@ncsu.edu>
2014-12-05 08:01:23 -0500

    fix footer

--- public/templates/footer.php
+++ public/templates/footer.php
@@ -5,14 +5,14 @@
 <div id="footer" class="navbar navbar-default">
 	<div class="container">
 		<p>
-			<small class="text-muted">Copright &copy; Angels of Death 2005-2014. All rights reserved.</small>
+			<small class="text-muted">Copright &copy; Angels of Death 2005-2015. All rights reserved.</small>
 			<small class="text-muted pull-right">Developed by Guybrush</small>
 		</p>
 	</div>
 </div>
 
-<input type="hidden" id="player" name="player" value="<?php echo $player; ?>" />
-<input type="hidden" id="game" name="game" value="<?php echo $gameid; ?>" />
+<!-- <input type="hidden" id="player" name="player" value="<?php //echo $player; ?>" />
+<input type="hidden" id="game" name="game" value="<?php //echo $gameid; ?>" /> -->
 
 <script type="text/javascript" src="/aod_rct/public/js/main.js"></script>
 
