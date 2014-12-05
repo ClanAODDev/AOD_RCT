00b425803d1292ca63b3c1c239f72c868cc5fe34
dcdeaton <dcdeaton@ncsu.edu>
2014-12-05 12:50:06 -0500

    add more content

--- application/views/home.php
+++ application/views/home.php
@@ -1,23 +1,31 @@
 <?php
 if (isLoggedIn()) {
 
-$out = NULL;
+	$out = NULL;
 
-$out .= "
-<div class='container fade-in margin-top-20'>
-	<div class='row'>
+	$out .= "
+	<div class='container fade-in margin-top-20'>
+		<div class='row'>
 
-		<div class='jumbotron'>
-			<h1>Howdy <strong>{$curUser}</strong>!</h1>
-			<p>This is the squad administration tool for the AOD organization. To get started, select an action from the shortcut menu below.</p>
+			<div class='jumbotron'>
+				<h1>Howdy <strong>{$curUser}</strong>!</h1>
+				<p>This is the squad administration tool for the AOD organization. Leaders will be able to manage individual squad members, view activity trends of their respective divisions, and clan leaders will be able to see activity clan-wide and across divisions.</p>
+			</div>
+
+			<div class='panel panel-warning'>
+				<div class='panel-heading'>
+					<strong>Under Construction</strong>
+				</div>
+				<div class='panel-body'>
+					Application is currently being developed. More tools and services will soon be available for use.
+				</div>
+			</div>
+
 		</div>
-
 	</div>
-</div>
-";
+	";
 
-
-echo $out;
+	echo $out;
 
 }
 
