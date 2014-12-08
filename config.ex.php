b504f124aa701874d8836ae5e028a40cad42af87
Chris Deaton <flashadvocate@gmail.com>
2014-12-08 11:09:27 -0500

    Update config.ex.php

--- config.ex.php
+++ config.ex.php
@@ -1,8 +1,9 @@
 <?php
 
-// config file needs to be located
-// above the root directory of aod_rct
-// ex. /public_html/.../
+/**
+ * configuration file
+ * for AOD_RCT
+ */
 
 define('DB_HOST', '');
 define('DB_NAME', '');
@@ -10,7 +11,22 @@ define('DB_USER', '');
 define('DB_PASS', '');
 
 // temporary until user management / credentials are introduced
-DEFINE('FORUM_USER', '');
-DEFINE('FORUM_PASS', '');
+define('FORUM_USER', '');
+define('FORUM_PASS', '');
 
+// show output
+define('SHOW_OUTPUT', false);
+
+// debug mode
+define('DEBUG_MODE', false);
+
+// defines for website URLs
+define('CLANAOD', 'http://www.clanaod.net/forums/member.php?u=');
+define('BATTLELOG', 'http://battlelog.battlefield.com/bf4/user/');
+define('BF4DB', 'http://bf4db.com/players/');
+define('BF4STATS', 'http://api.bf4stats.com/api/playerInfo?plat=pc&opt=names&output=lines&name=');
+
+// defines for BF4 division activity status display
+define('PERCENTAGE_CUTOFF_GREEN', 75);
+define('PERCENTAGE_CUTOFF_AMBER', 50);
 ?>
