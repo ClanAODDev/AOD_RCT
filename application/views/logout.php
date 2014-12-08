c827d6e25f868cf0518e70e910236c1e0aac3926
Chris Deaton <dcdeaton@ncsu.edu>
2014-12-08 12:23:04 -0500

    update logout page

--- application/views/logout.php
+++ application/views/logout.php
@@ -6,17 +6,24 @@ $_SESSION = array();
 // If it's desired to kill the session, also delete the session cookie.
 // Note: This will destroy the session, and not just the session data!
 if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
+	$params = session_get_cookie_params();
+	setcookie(session_name(), '', time() - 42000,
+		$params["path"], $params["domain"],
+		$params["secure"], $params["httponly"]
+		);
 }
 
 // Finally, destroy the session.
 session_destroy();
 ?>
 
-<h1>Logged out</h1>
+<div class='container fade-in margin-top-20'>
+	<div class='row'>
+		<div class='col-md-12'>
+		<h1>Logged out</strong>!</h1>
+			<p>Your session has been destroyed.</p>
+		</div> <!-- end col -->
+	</div> <!-- end end row -->
+</div>
 
 
