4e348a2c1264c0f9b027e05e1b33f6a6b8b79ef1
Chris Deaton <dcdeaton@ncsu.edu>
2014-12-08 18:26:59 -0500

    add github link to footer

--- public/templates/footer.php
+++ public/templates/footer.php
@@ -4,7 +4,7 @@
 
 <div id="footer" class="navbar navbar-default">
 	<div class="container">
-			<small class="text-muted col-xs-6">Copright &copy; Angels of Death <span class="hidden-xs">2005-2015. All rights reserved.</span><br /><span class="hidden-xs">Developed by Guybrush, Sc0rp10n66</span></small>
+			<small class="text-muted col-xs-6">Copright &copy; Angels of Death <span class="hidden-xs">2005-2015. All rights reserved.</span><br /><span class="hidden-xs"> Found a bug or have a suggestion? Shoot it to us on <a href="https://github.com/flashadvocate/AOD_RCT/issues"><strong>GitHub</strong></a></span></small>
 			<small class="text-muted col-xs-6 text-right userList"><img src="https://aod.sitespot.com/public/images/loading_2.gif" /> Loading users...</small>
 	</div>
 </div>
