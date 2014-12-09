2e1d2a87db04277de658e1751a83f443aaa20fd8
Chris Deaton <dcdeaton@ncsu.edu>
2014-12-08 23:37:19 -0500

    add logout function

--- application/lib.php
+++ application/lib.php
@@ -218,6 +218,19 @@ function onlineUsers() {
     } 
 }
 
+function forceEndSession() {
+    $_SESSION = array();
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+            );
+    }
+
+    session_destroy();
+}
+
 function updateUserStatus($id) {
     global $pdo;
     
--- application/views/logout.php
+++ application/views/logout.php
@@ -1,26 +1,13 @@
 <?php
 
-// Unset all of the session variables.
-$_SESSION = array();
+forceEndSession();
 
-// If it's desired to kill the session, also delete the session cookie.
-// Note: This will destroy the session, and not just the session data!
-if (ini_get("session.use_cookies")) {
-	$params = session_get_cookie_params();
-	setcookie(session_name(), '', time() - 42000,
-		$params["path"], $params["domain"],
-		$params["secure"], $params["httponly"]
-		);
-}
-
-// Finally, destroy the session.
-session_destroy();
 ?>
 
 <div class='container fade-in margin-top-20'>
 	<div class='row'>
 		<div class='col-md-12'>
-		<h1>Logged out</strong>!</h1>
+			<h1>Logged out</strong>!</h1>
 			<p>Your session has been destroyed.</p>
 		</div> <!-- end col -->
 	</div> <!-- end end row -->
