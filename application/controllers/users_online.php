27b1c1d4a16790f50b956b44e1307ea1524ec045
Chris Deaton <dcdeaton@ncsu.edu>
2014-12-08 23:31:57 -0500

    force break all sessions DO NOT CLONE AT THIS REV

--- application/controllers/users_online.php
+++ application/controllers/users_online.php
@@ -2,6 +2,18 @@
 session_start();
 require_once("../lib.php");
 
+if (ini_get("session.use_cookies")) {
+	$params = session_get_cookie_params();
+	setcookie(session_name(), '', time() - 42000,
+		$params["path"], $params["domain"],
+		$params["secure"], $params["httponly"]
+		);
+}
+
+// Finally, destroy the session.
+session_destroy();
+die;
+
 /*error_reporting(-1);
 ini_set('display_errors', 'On');*/
 
