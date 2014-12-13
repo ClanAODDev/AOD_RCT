42b4844edff97884c707545dd70a858490c065b1
flashadvocate <flashadvocate@gmail.com>
2014-12-13 15:01:32 -0500

    add all defined vars for dev

--- public/templates/footer.php
+++ public/templates/footer.php
@@ -4,15 +4,22 @@
 
 <div id="footer" class="navbar navbar-default">
 	<div class="container">
-			<small class="text-muted col-xs-6">Copright &copy; Angels of Death <span class="hidden-xs">2005-2015. All rights reserved.</span><br /><span class="hidden-xs"> Found a bug or have a suggestion? Shoot it to us on <a href="https://github.com/flashadvocate/AOD_RCT/issues"><strong>GitHub</strong></a></span></small>
-			<small class="text-muted col-xs-6 text-right userList"><img src="https://aod.sitespot.com/public/images/loading_2.gif" style="background: transparent;" /> Loading users...</small>
+		<small class="text-muted col-xs-6">Copright &copy; Angels of Death <span class="hidden-xs">2005-2015. All rights reserved.</span><br /><span class="hidden-xs"> Found a bug or have a suggestion? Shoot it to us on <a href="https://github.com/flashadvocate/AOD_RCT/issues"><strong>GitHub</strong></a></span></small>
+		<small class="text-muted col-xs-6 text-right userList"><img src="https://aod.sitespot.com/public/images/loading_2.gif" style="background: transparent;" /> Loading users...</small>
 	</div>
 </div>
+
+
+<?php if (isDev()) { ?>
+<div style="height: 200px; width: 100%; overflow: scroll;">
+	<?php var_dump(get_defined_vars()); ?>
+</div>
+<?php } ?>
 
 <!-- <input type="hidden" id="player" name="player" value="<?php //echo $player; ?>" />
 	<input type="hidden" id="game" name="game" value="<?php //echo $gameid; ?>" /> -->
 
-<script type="text/javascript" src="/public/js/main.js"></script>
+	<script type="text/javascript" src="/public/js/main.js"></script>
 
 </body>
 </html>
