e5e042f7a6dc7b676400a808712bf5746c370212
dcdeaton <dcdeaton@ncsu.edu>
2015-01-05 13:14:13 -0500

    check last 5 pages of thread (rct tool)
    in the event a new recruit's post is not on the last page (in the case
    of clanwide threads, which change by the minute), recursively check the
    next to last page until a match is found. Max of five tries, to prevent
    traffic load. This should never fail if the user posted that day.
    
    closes #39

--- application/lib.php
+++ application/lib.php
@@ -730,16 +730,77 @@ function validatePassword($pass, $user)
     
 }
 
+function curl_last_url(/*resource*/ $ch, /*int*/ &$maxredirect = null) { 
+    $mr = $maxredirect === null ? 5 : intval($maxredirect); 
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false); 
+    if ($mr > 0) { 
+
+        $newurl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL); 
+
+        $rch = curl_copy_handle($ch); 
+        curl_setopt($rch, CURLOPT_HEADER, true); 
+        curl_setopt($rch, CURLOPT_NOBODY, true); 
+        curl_setopt($rch, CURLOPT_FORBID_REUSE, false); 
+        curl_setopt($rch, CURLOPT_RETURNTRANSFER, true); 
+        do { 
+            curl_setopt($rch, CURLOPT_URL, $newurl); 
+            $header = curl_exec($rch); 
+            if (curl_errno($rch)) { 
+                $code = 0; 
+            } else { 
+                $code = curl_getinfo($rch, CURLINFO_HTTP_CODE); 
+                // echo $code;
+                if ($code == 301 || $code == 302) { 
+                    preg_match('/Location:(.*?)\n/', $header, $matches); 
+                    $newurl = trim(array_pop($matches)); 
+                } else { 
+                    $code = 0; 
+                } 
+            } 
+        } while ($code && --$mr); 
+        curl_close($rch); 
+        if (!$mr) { 
+            if ($maxredirect === null) { 
+                trigger_error('Too many redirects. When following redirects, libcurl hit the maximum amount.', E_USER_WARNING); 
+            } else { 
+                $maxredirect = 0; 
+            } 
+            return false; 
+        } 
+        curl_setopt($ch, CURLOPT_URL, $newurl); 
+    } 
+    return $newurl; 
+}
+
 function checkThread($player, $thread)
 {
 
     $ch = curl_init();
     
-    curl_setopt($ch, CURLOPT_URL, $thread . "&page=9999999");
+    curl_setopt($ch, CURLOPT_URL, $thread . "&goto=newpost");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     $getPosts   = curl_exec($ch);
     $countPosts = stripos($getPosts, $player);
+
+    // if not found on the last page, check recursively (max 5 pages) until a match is found
+    // don't want to go over 5 pages because of high traffic.
+    // may change this to 3...
+    if (!$countPosts) {
+
+        $url = parse_url(curl_last_url($ch));
+        $query = $url['query'];
+        parse_str($query, $url_array);
+        $page = $url_array['page']-1;
+
+        curl_setopt($ch, CURLOPT_URL, $thread . "&page={$page}");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        $getPosts   = curl_exec($ch);
+        $countPosts = stripos($getPosts, $player);
+
+    }
+
     if ($countPosts) {
         return true;
     } else {
