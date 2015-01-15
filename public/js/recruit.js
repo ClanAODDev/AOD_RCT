132531f6b075f23a2141950ac063c4e1602f8794
Chris Deaton <dcdeaton@ncsu.edu>
2015-01-15 18:36:03 -0500

    remove popup function from rct

--- public/js/recruit.js
+++ public/js/recruit.js
@@ -185,11 +185,6 @@ function loadThreadCheck() {
             window.open($(this).attr("href" + member_id), "popupWindow", "width=1000,height=600,scrollbars=yes");
         });
 
-        // popup link
-        $(".popup-link").click(function(e) {
-            e.preventDefault();
-            window.open($(this).attr("href"), "popupWindow", "width=1090,height=600,scrollbars=yes");
-        });
     }
 
     if (player) {
