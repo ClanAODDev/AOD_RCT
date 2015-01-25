6b6535544bc7ec286066083daf05489e3fce51d8
Chris Deaton <dcdeaton@ncsu.edu>
2015-01-25 18:13:51 -0500

    add content to help

--- application/views/help.php
+++ application/views/help.php
@@ -20,10 +20,48 @@
 		<p>The squad tracker for the AOD community was originally intended as a way for squad leaders to keep track of their squad's activity, to faciliate more effective management and to ensure that promotions and, conversely, inactivity were recognized evenly across the division. Since that point, the tracker concept has evolved into an application that can be used across the division, and hopefully across multiple divisions, to offer a bird's-eye view of the overall health and activeness of a particular game in AOD.</p>
 
 
-		<div class='col-md-8'>
-			<div class='margin-top-20'>
+		
+		<div class='margin-top-20'>
+			<div id="faq">
 
+				<div class="panel-group" id="accordion">
+
+
+
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"	href="#collapse-1"><h4 class="panel-title">
+								Lorem ipsum dolor sit amet?
+							</h4></a>
+						</div>
+
+						<div id="collapse-1" class="panel-collapse collapse">
+							<div class="panel-body">
+								<p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
+									squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa
+									nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
+									single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft
+								</p>
+								<p>beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
+									lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you
+									probably haven't heard of them accusamus labore sustainable VHS.
+								</p>
+							</div>
+
+							<div class="panel-footer">
+								<div class="btn-group btn-group-xs text-muted"><span>Is there an error with this question? Let us know!</span></div>
+								<div class="btn-group btn-group-xs pull-right">
+									<a class="btn btn-primary" href="#">Report this question</a>
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+
+				</div>
 			</div>
+			
 		</div>
 
 	</div>
