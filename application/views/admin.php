9438fb2fc62696a57fc28ef64d64500f2dc4a46f
Chris Deaton <dcdeaton@ncsu.edu>
2015-02-03 00:01:03 -0500

    some fixes to admin page -> tabs
    also add tab stuff, header

--- application/views/admin.php
+++ application/views/admin.php
@@ -12,39 +12,47 @@ if ($userRole > 1) {
 
 ?>
 
-<div class="row">
-	<div class="col-sm-2">
-		<div class="sidebar-nav">
-			<div class="navbar navbar-default" role="navigation">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<span class="visible-xs navbar-brand">Sidebar menu</span>
-				</div>
-				<div class="navbar-collapse collapse sidebar-navbar-collapse">
-					<ul class="nav navbar-nav">
-						<li class="active"><a href="#">Member Email Tool</a></li>
-						<li class="active"><a href="#">Future Tool</a></li>
-						<li class="active"><a href="#">Future Tool</a></li>
-						<li class="active"><a href="#">Future Tool</a></li>
-					</ul>
-				</div><!--/.nav-collapse -->
-			</div>
-		</div>
+
+<div class='container fade-in'>
+
+
+	<div class='page-header'>
+		<h3><strong>Admin Functions</strong></h3>
 	</div>
 
-	<div class='container fade-in'>
-		<div class="col-sm-9">
-			<div class='page-header'>
-				<h1><strong>Email Active Member</strong></h1>
+	<div class="row">
+		<div class="col-sm-2">
+			<div class="sidebar-nav">
+				<div class="navbar navbar-default" role="navigation">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<span class="visible-xs navbar-brand">Sidebar menu</span>
+					</div>
+					<div class="navbar-collapse collapse sidebar-navbar-collapse">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#email-form" role="tab" data-toggle="tab">Member Email Tool</a></li>
+							<li class="active"><a href="#tab2" role="tab" data-toggle="tab">Future Tool</a></li>
+							<li class="active"><a href="#tab3" role="tab" data-toggle="tab">Future Tool</a></li>
+							<li class="active"><a href="#tab4" role="tab" data-toggle="tab">Future Tool</a></li>
+						</ul>
+					</div><!--/.nav-collapse -->
+				</div>
 			</div>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-8">
+		</div>
+
+		<div class="col-sm-9">
+
+			<div class="tab-content">
+
+
+				<div role="tabpanel" class="tab-pane active" id="email-form">
+
+					<div class="col-md-12">
 						<div class="well well-sm">
 							<form class="form-horizontal" action="" method="post">
 								<fieldset>
@@ -79,12 +87,47 @@ if ($userRole > 1) {
 											<button type="reset" class="btn btn-secondary btn-lg">Reset</button>
 										</div>
 									</div>
+
 								</fieldset>
 							</form>
 						</div>
-					</div>
-				</div>
+
+					</div><!-- end col md 12 -->
+
+				</div><!-- end email form-->
+
+
+
+
+
+
+
+				<div role="tabpanel" class="tab-pane" id="tab2">
+
+					asdfasdfasdfasdf
+
+				</div><!-- end tab 2 -->
+
+
+
+
+
+
+				<div role="tabpanel" class="tab-pane" id="tab3">
+
+					asdfasdfasdfasdf
+
+				</div><!-- end tab 3 -->
+
+
+
+
 			</div>
-		</div>	
-	</div>
-</div>
+
+		</div><!-- end tab content-->
+
+	</div><!-- end row -->
+
+
+
+</div><!-- end container -->
