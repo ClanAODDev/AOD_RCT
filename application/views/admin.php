f874c11f4d9e6417816fd7c5806884e36d13eaa0
Chris Deaton <dcdeaton@ncsu.edu>
2015-02-03 02:01:54 -0500

    refactor output

--- application/views/admin.php
+++ application/views/admin.php
@@ -2,6 +2,175 @@
 
 if ($userRole > 1) {
 	
+	$breadcrumb = "
+	<ul class='breadcrumb'>
+		<li><a href='/'>Home</a></li>
+		<li class='active'>Administration</li>
+	</ul>";
+
+
+	$out .= "
+	<div class='container fade-in'>
+
+		{$breadcrumb}
+
+
+		<div class='page-header'>
+			<h1>Administration Panel</h1>
+
+
+		</div>
+		<p>Several administrative functions exist to manage the application settings, perform admin tasks such as contacting members via email, and updating application data on demand. These tools are subject to change and may be unavailable without prior warning. Please do not use a tool if you are not completely certain of its purpose.</p>
+
+
+
+		<div class='page-header margin-top-50'>
+			<h3>Admin Tools</h3>
+		</div>
+
+		<div class='row'>
+			<div class='col-sm-2'>
+				<div class='sidebar-nav'>
+					<div class='navbar navbar-default' role='navigation'>
+						<div class='navbar-header'>
+							<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='.sidebar-navbar-collapse'>
+								<span class='sr-only'>Toggle navigation</span>
+								<span class='icon-bar'></span>
+								<span class='icon-bar'></span>
+								<span class='icon-bar'></span>
+							</button>
+							<span class='visible-xs navbar-brand'>Sidebar menu</span>
+						</div>
+						<div class='navbar-collapse collapse sidebar-navbar-collapse'>
+							<ul class='nav navbar-nav'>
+								<li class='active'><a href='#email-form' role='tab' data-toggle='tab'>Member Email Tool</a></li>
+								<li class=''><a href='#tab2' role='tab' data-toggle='tab'>Future Tool</a></li>
+								<li class=''><a href='#tab3' role='tab' data-toggle='tab'>Future Tool</a></li>
+								<li class=''><a href='#tab4' role='tab' data-toggle='tab'>Future Tool</a></li>
+							</ul>
+						</div><!--/.nav-collapse -->
+					</div>
+
+					<div class='clearfix visible-xs'></div>
+				</div>
+			</div>
+
+			<div class='col-sm-10'>
+
+				<div class='tab-content'>
+
+					<div role='tabpanel' class='tab-pane active' id='email-form'>
+
+
+
+
+						<div class='col-md-12'>
+
+							<form class='form' action='' method='post'>
+
+
+
+								<div class='panel panel-default'>
+
+
+
+
+
+									<div class='panel-heading'><i class='fa fa-envelope'></i> Send email</div>
+									<div class='panel-body'>
+
+
+
+										<!-- Email input-->
+										<div class='form-group'>
+											<label class='control-label' for='email'>Member E-mail</label>
+											<input id='email' name='email' type='text' placeholder='Member email' class='form-control'>
+										</div>
+
+										<!-- Subject line -->
+										<div class='form-group'>
+											<label class='ccontrol-label' for='subject'>Subject</label>
+											<input class='form-control' id='subject' name='subject' placeholder='Subject Line' rows='1'></input>
+										</div>
+
+										<!-- Message body -->
+										<div class='form-group'>
+											<label class='control-label' for='message'>Your message</label>
+											<textarea class='form-control' id='message' name='message' placeholder='Please enter your message here...' rows='5'></textarea>
+										</div>
+
+
+
+									</div>
+
+
+
+									<div class='panel-footer clearfix'>
+										<div class='form-group'>
+											<div class='col-md-12 text-right'>
+												<button type='reset' class='btn btn-secondary'>Reset</button>
+												<button type='submit' class='btn btn-primary'>Submit</button>
+											</div>
+										</div>
+									</div>
+
+
+
+
+
+								</div><!-- end panel -->
+
+
+							</form>
+
+						</div><!-- end col md 12 -->
+
+					</div><!-- end email form-->
+
+
+
+
+
+
+
+					<div role='tabpanel' class='tab-pane' id='tab2'>
+
+						asdfasdfasdfasdf1
+
+					</div><!-- end tab 2 -->
+
+
+
+
+
+
+					<div role='tabpanel' class='tab-pane' id='tab3'>
+
+						asdfasdfasdfasdf2
+
+					</div><!-- end tab 3 -->
+
+
+
+
+
+
+					<div role='tabpanel' class='tab-pane' id='tab4'>
+
+						asdfasdfasdfasdf3
+
+					</div><!-- end tab 4 -->
+
+
+				</div>
+
+			</div><!-- end tab content-->
+
+		</div><!-- end row -->
+
+
+
+	</div><!-- end container -->";
 
 
 
@@ -10,153 +179,6 @@ if ($userRole > 1) {
 }
 
 
+
+echo $out;
 ?>
-
-
-<div class='container fade-in'>
-
-
-	<div class='page-header'>
-		<h3><strong>Administration</strong></h3>
-	</div>
-	<div class="row">
-		<div class="col-sm-2">
-			<div class="sidebar-nav">
-				<div class="navbar navbar-default" role="navigation">
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<span class="visible-xs navbar-brand">Sidebar menu</span>
-					</div>
-					<div class="navbar-collapse collapse sidebar-navbar-collapse">
-						<ul class="nav navbar-nav">
-							<li class="active"><a href="#email-form" role="tab" data-toggle="tab">Member Email Tool</a></li>
-							<li class=""><a href="#tab2" role="tab" data-toggle="tab">Future Tool</a></li>
-							<li class=""><a href="#tab3" role="tab" data-toggle="tab">Future Tool</a></li>
-							<li class=""><a href="#tab4" role="tab" data-toggle="tab">Future Tool</a></li>
-						</ul>
-					</div><!--/.nav-collapse -->
-				</div>
-			</div>
-		</div>
-
-		<div class="col-sm-10">
-
-			<div class="tab-content">
-
-				<div role="tabpanel" class="tab-pane active" id="email-form">
-
-
-
-
-					<div class="col-md-12">
-
-						<form class="form" action="" method="post">
-
-
-
-							<div class="panel panel-default">
-
-
-
-
-
-								<div class="panel-heading"><i class="fa fa-envelope"></i> Send email</div>
-								<div class="panel-body">
-
-
-
-									<!-- Email input-->
-									<div class="form-group">
-										<label class="control-label" for="email">Member E-mail</label>
-										<input id="email" name="email" type="text" placeholder="Member email" class="form-control">
-									</div>
-
-									<!-- Subject line -->
-									<div class="form-group">
-										<label class="ccontrol-label" for="subject">Subject</label>
-										<input class="form-control" id="subject" name="subject" placeholder="Subject Line" rows="1"></input>
-									</div>
-
-									<!-- Message body -->
-									<div class="form-group">
-										<label class="control-label" for="message">Your message</label>
-										<textarea class="form-control" id="message" name="message" placeholder="Please enter your message here..." rows="5"></textarea>
-									</div>
-
-
-
-								</div>
-
-
-
-								<div class="panel-footer clearfix">
-									<div class="form-group">
-										<div class="col-md-12 text-right">
-											<button type="reset" class="btn btn-secondary">Reset</button>
-											<button type="submit" class="btn btn-primary">Submit</button>
-										</div>
-									</div>
-								</div>
-
-
-
-
-
-							</div><!-- end panel -->
-
-
-						</form>
-
-					</div><!-- end col md 12 -->
-
-				</div><!-- end email form-->
-
-
-
-
-
-
-
-				<div role="tabpanel" class="tab-pane" id="tab2">
-
-					asdfasdfasdfasdf1
-
-				</div><!-- end tab 2 -->
-
-
-
-
-
-
-				<div role="tabpanel" class="tab-pane" id="tab3">
-
-					asdfasdfasdfasdf2
-
-				</div><!-- end tab 3 -->
-
-
-
-
-
-
-				<div role="tabpanel" class="tab-pane" id="tab4">
-
-					asdfasdfasdfasdf3
-
-				</div><!-- end tab 4 -->
-
-
-			</div>
-
-		</div><!-- end tab content-->
-
-	</div><!-- end row -->
-
-
-
-</div><!-- end container -->
