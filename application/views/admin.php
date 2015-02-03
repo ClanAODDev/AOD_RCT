de3d4fb8162dd175769a767d6f573345e2c4abc9
ichigobankai09 <nickacosta93@gmail.com>
2015-02-03 00:29:01 -0500

    some more fixes to admin page -> tabs

--- application/views/admin.php
+++ application/views/admin.php
@@ -19,7 +19,6 @@ if ($userRole > 1) {
 	<div class='page-header'>
 		<h3><strong>Admin Functions</strong></h3>
 	</div>
-
 	<div class="row">
 		<div class="col-sm-2">
 			<div class="sidebar-nav">
@@ -36,9 +35,9 @@ if ($userRole > 1) {
 					<div class="navbar-collapse collapse sidebar-navbar-collapse">
 						<ul class="nav navbar-nav">
 							<li class="active"><a href="#email-form" role="tab" data-toggle="tab">Member Email Tool</a></li>
-							<li class="active"><a href="#tab2" role="tab" data-toggle="tab">Future Tool</a></li>
-							<li class="active"><a href="#tab3" role="tab" data-toggle="tab">Future Tool</a></li>
-							<li class="active"><a href="#tab4" role="tab" data-toggle="tab">Future Tool</a></li>
+							<li class=""><a href="#tab2" role="tab" data-toggle="tab">Future Tool</a></li>
+							<li class=""><a href="#tab3" role="tab" data-toggle="tab">Future Tool</a></li>
+							<li class=""><a href="#tab4" role="tab" data-toggle="tab">Future Tool</a></li>
 						</ul>
 					</div><!--/.nav-collapse -->
 				</div>
@@ -48,7 +47,6 @@ if ($userRole > 1) {
 		<div class="col-sm-9">
 
 			<div class="tab-content">
-
 
 				<div role="tabpanel" class="tab-pane active" id="email-form">
 
@@ -82,7 +80,7 @@ if ($userRole > 1) {
 
 
 									<div class="form-group">
-										<div class="col-md-12 text-center">
+										<div class="col-md-12 text-right">
 											<button type="submit" class="btn btn-primary btn-lg">Submit</button>
 											<button type="reset" class="btn btn-secondary btn-lg">Reset</button>
 										</div>
@@ -104,7 +102,7 @@ if ($userRole > 1) {
 
 				<div role="tabpanel" class="tab-pane" id="tab2">
 
-					asdfasdfasdfasdf
+					asdfasdfasdfasdf1
 
 				</div><!-- end tab 2 -->
 
@@ -115,11 +113,20 @@ if ($userRole > 1) {
 
 				<div role="tabpanel" class="tab-pane" id="tab3">
 
-					asdfasdfasdfasdf
+					asdfasdfasdfasdf2
 
 				</div><!-- end tab 3 -->
 
 
+
+
+
+
+				<div role="tabpanel" class="tab-pane" id="tab4">
+
+					asdfasdfasdfasdf3
+
+				</div><!-- end tab 4 -->
 
 
 			</div>
