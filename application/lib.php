0b1a3531b27251ee9d1ff80b224babe657413b4d
Chris Deaton <dcdeaton@ncsu.edu>
2015-02-10 16:32:59 -0500

    synchronize mysql and php timezone offsets

--- application/lib.php
+++ application/lib.php
@@ -224,10 +224,17 @@ function dbConnect()
     global $pdo;
     $conn = '';
 
+    $now = new DateTime();
+    $mins = $now->getOffset() / 60;
+    $sgn = ($mins < 0 ? -1 : 1);
+    $mins = abs($mins);
+    $hrs = floor($mins / 60);
+    $mins -= $hrs * 60;
+    $offset = sprintf('%+d:%02d', $hrs*$sgn, $mins);
 
     try {
         $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
-
+        $pdo->exec("SET time_zone='$offset';");
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     
