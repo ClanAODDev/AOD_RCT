1770926db07d9438691b5c7be15d6edfa45e1ff9
Chris Deaton <dcdeaton@ncsu.edu>
2015-02-10 07:56:57 -0500

    add error handling, error bg to toplist stat img

--- stats/top10.png/index.php
+++ stats/top10.png/index.php
@@ -2,116 +2,128 @@
 header('Content-Type: image/png');
 include("../../application/lib.php");
 
-$im = imagecreatetruecolor(900,330);
+$im = imagecreatetruecolor(900, 330);
 
 // color
-$white = imagecolorallocate($im, 255, 255, 255);
-$grey = imagecolorallocate($im, 128, 128, 128);
-$darkergrey = imagecolorallocate($im, 30,30,30);
-$orange = imagecolorallocate($im, 255,108,0);
-$darkGrey = imagecolorallocate($im, 50,50,50);
-$im = imagecreatefrompng("../images/big-bg.png");
+$white      = imagecolorallocate($im, 255, 255, 255);
+$grey       = imagecolorallocate($im, 128, 128, 128);
+$darkergrey = imagecolorallocate($im, 30, 30, 30);
+$orange     = imagecolorallocate($im, 255, 108, 0);
+$darkGrey   = imagecolorallocate($im, 50, 50, 50);
+$im         = imagecreatefrompng("../images/big-bg.png");
 
-$text = "Battlefield Division";
-$datesub = "AOD games / Total games";
+$text     = "Battlefield Division";
+$datesub  = "AOD games / Total games";
 $dateText = date('d M', strtotime('-30 days')) . "-" . date('d M');
 
-$tinyfont = "../fonts/copy0855.ttf";
+$tinyfont     = "../fonts/copy0855.ttf";
 $tinyboldfont = "../fonts/copy0866.ttf";
-$bigfont = "../fonts/din-black.otf";
+$bigfont      = "../fonts/din-black.otf";
 
 // x value positions
 $games_col_1 = 190;
-$num_col_1 = 23;
-$name_col_1 = 45;
+$num_col_1   = 23;
+$name_col_1  = 45;
 
 $games_col_2 = 465;
-$num_col_2 = 300;
-$name_col_2 = 320;
+$num_col_2   = 300;
+$name_col_2  = 320;
 
 $total_percent_x = 590;
 $total_percent_y = 160;
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
 /**
  * get data
  */
+try {
 
-$daily = get_division_toplist("daily", 10);
-$monthly = get_division_toplist("monthly", 10);
+	$daily   = get_division_toplist("dailyd", 10);
+	$monthly = get_division_toplist("monthly", 10);
 
-
-
-/**
- * create elements
- */
-
-// date
-imagettftext($im, 6, 0, 715, 240, $darkGrey, $tinyfont, strtoupper($dateText));
-imagettftext($im, 6, 0, 612, 100, $darkergrey, $tinyfont, strtoupper($datesub));
+	if (!$daily || !$monthly) {
+		throw new Exception("Query could not be completed", 1);
+	}
 
 
-// daily stats
 
-$y = 65;
-$i = 1;
+    /**
+     * create elements
+     */
+    
+    // date
+    imagettftext($im, 6, 0, 715, 240, $darkGrey, $tinyfont, strtoupper($dateText));
+    imagettftext($im, 6, 0, 612, 100, $darkergrey, $tinyfont, strtoupper($datesub));
+    
+    
+    // daily stats
+    
+    $y = 65;
+    $i = 1;
+    
+    imagettftext($im, 6, 0, $num_col_1, $y, $orange, $tinyfont, strtoupper("#"));
+    imagettftext($im, 6, 0, $name_col_1, $y, $orange, $tinyfont, strtoupper("Player"));
+    imagettftext($im, 6, 0, $games_col_1, $y, $orange, $tinyfont, strtoupper("AOD Games"));
+    
+    foreach ($daily['players'] as $player) {
+    	$y    = $y + 20;
+    	$name = strtoupper($player['rank']) . " " . strtoupper($player['forum_name']);
+        // number
+    	imagettftext($im, 6, 0, $num_col_1, $y, $orange, $tinyfont, "{$i}.");
+        // name
+    	imagettftext($im, 6, 0, $name_col_1, $y, $white, $tinyfont, $name);
+        // games
+    	imagettftext($im, 6, 0, $games_col_1, $y, $white, $tinyboldfont, "{$player['aod_games']}");
+    	$i++;
 
-imagettftext($im, 6, 0, $num_col_1, $y, $orange, $tinyfont, strtoupper("#"));
-imagettftext($im, 6, 0, $name_col_1, $y, $orange, $tinyfont, strtoupper("Player"));
-imagettftext($im, 6, 0, $games_col_1, $y, $orange, $tinyfont, strtoupper("AOD Games"));
+    }
+    
+    
+    // monthly stats
+    
+    $y = 65;
+    $i = 1;
+    
+    imagettftext($im, 6, 0, $num_col_2, $y, $orange, $tinyfont, strtoupper("#"));
+    imagettftext($im, 6, 0, $name_col_2, $y, $orange, $tinyfont, strtoupper("Player"));
+    imagettftext($im, 6, 0, $games_col_2, $y, $orange, $tinyfont, strtoupper("AOD Games"));
+    
+    foreach ($monthly['players'] as $player) {
+    	$y    = $y + 20;
+    	$name = strtoupper($player['rank']) . " " . strtoupper($player['forum_name']);
+        // number
+    	imagettftext($im, 6, 0, $num_col_2, $y, $orange, $tinyfont, "{$i}.");
+        // name
+    	imagettftext($im, 6, 0, $name_col_2, $y, $white, $tinyfont, $name);
+        // games
+    	imagettftext($im, 6, 0, $games_col_2, $y, $white, $tinyboldfont, "{$player['aod_games']}");
+    	$i++;
 
-foreach ($daily['players'] as $player) {
-	$y = $y+20;
-	$name = strtoupper($player['rank']) . " " . strtoupper($player['forum_name']);
- 	// number
-	imagettftext($im, 6, 0, $num_col_1, $y, $orange, $tinyfont, "{$i}.");
-	// name
-	imagettftext($im, 6, 0, $name_col_1, $y, $white, $tinyfont, $name);
-	// games
-	imagettftext($im, 6, 0, $games_col_1, $y, $white, $tinyboldfont, "{$player['aod_games']}");
-	$i++;
+    }
+    
+    // total aod games stat
+    
+    $total = $monthly['total_percentage'];
+    imagettftext($im, 48, 0, $total_percent_x, $total_percent_y, $white, $bigfont, $total . "%");
+    
+}
+catch (Exception $e) {
+
+	$im = imagecreatefrompng("../images/big-bg-error.png");
+	imagettftext($im, 6, 0, 10, 320, $darkGrey, $tinyfont, strtoupper($e->getMessage()));
 
 }
-
-
-// monthly stats
-
-$y = 65;
-$i = 1;
-
-imagettftext($im, 6, 0, $num_col_2, $y, $orange, $tinyfont, strtoupper("#"));
-imagettftext($im, 6, 0, $name_col_2, $y, $orange, $tinyfont, strtoupper("Player"));
-imagettftext($im, 6, 0, $games_col_2, $y, $orange, $tinyfont, strtoupper("AOD Games"));
-
-foreach ($monthly['players'] as $player) {
-	$y = $y+20;
-	$name = strtoupper($player['rank']) . " " . strtoupper($player['forum_name']);
- 	// number
-	imagettftext($im, 6, 0, $num_col_2, $y, $orange, $tinyfont, "{$i}.");
-	// name
-	imagettftext($im, 6, 0, $name_col_2, $y, $white, $tinyfont, $name);
-	// games
-	imagettftext($im, 6, 0, $games_col_2, $y, $white, $tinyboldfont, "{$player['aod_games']}");
-	$i++;
-
-}
-
-
-
-// total aod games stat
-
-$total = $monthly['total_percentage'];
-imagettftext($im, 48, 0, $total_percent_x, $total_percent_y, $white, $bigfont, $total . "%");
-
 
 
 imagepng($im);
 imagepng($im, "../toplist-cache.png");
-imagedestroy($im); 
+imagedestroy($im);
 
 /*
 
- */
+*/
 
 
 ?>
