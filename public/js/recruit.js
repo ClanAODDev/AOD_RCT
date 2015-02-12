f4a80c3fd43cf3a0e6d500bcfda2fc18eaf3480e
Chris Deaton <dcdeaton@ncsu.edu>
2015-02-12 17:06:46 -0500

    prevent immediate reload of recruiting pages

--- public/js/recruit.js
+++ public/js/recruit.js
@@ -260,3 +260,9 @@ function windowOpener(url, name, args) {
 
     popupWin.focus(); 
  }
+
+
+ 
+window.onbeforeunload = function() {
+    return "You are in the recruitment process! Are you sure you want to refresh? You may lose any data entered.";
+}
