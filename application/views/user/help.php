d6f67c0fbc1600efdeebe3f2f5055e3b18926310
gofishAOD <nicholas@dykhuizen.com>
2015-02-25 19:44:32 -0800

    Test
    Test

--- application/views/user/help.php
+++ application/views/user/help.php
@@ -17,7 +17,7 @@
 
 
 	<div class='row'>
-		<p>The squad tracker for the AOD community was originally intended as a way for squad leaders to keep track of their squad's activity, to faciliate more effective management and to ensure that promotions and, conversely, inactivity were recognized evenly across the division. Since that point, the tracker concept has evolved into an application that can be used across the division, and hopefully across multiple divisions, to offer a bird's-eye view of the overall health and activeness of a particular game in AOD.</p>
+		<p>HI The squad tracker for the AOD community was originally intended as a way for squad leaders to keep track of their squad's activity, to faciliate more effective management and to ensure that promotions and, conversely, inactivity were recognized evenly across the division. Since that point, the tracker concept has evolved into an application that can be used across the division, and hopefully across multiple divisions, to offer a bird's-eye view of the overall health and activeness of a particular game in AOD.</p>
 
 
 		
