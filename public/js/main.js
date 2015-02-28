239fca65d7ab0979b46691c5aedaecc9aa498027
Chris Deaton <dcdeaton@ncsu.edu>
2015-02-28 01:08:45 -0500

    merge js, fix prog-bar, fix search results

--- public/js/main.js
+++ public/js/main.js
@@ -383,3 +383,408 @@ function selectText(containerid) {
         window.getSelection().addRange(range);
     }
 }
+
+
+$(".draggable").draggable({
+    connectToSortable: 'ul',
+    revert: 'invalid',
+    scroll: true,
+    scrollSensitivity: 100
+});
+
+
+
+$(".view-profile").click(function() {
+    var userId = $(this).closest('.list-group-item').attr('data-user-id');
+    location.href = "/member/" + userId;
+});
+
+var itemMoved, targetplatoon, sourcePlatoon, action = null;
+$(".sortable").sortable({
+    revert: true,
+    connectWith: 'ul',
+    placeholder: "ui-state-highlight",
+    receive: function(event, ui) {
+        itemMoved = $(ui.item).attr('data-member-id');
+        targetList = $(this).attr('id');
+
+        if (targetList == "flagged-inactives") {
+            $(ui.item).find('.removed-by').show().html("Flagged by you");
+            action = 1;
+            context = " flagged for removal.";
+
+            var flagCount = parseInt($(".flagCount").text()) + 1,
+                inactiveCount = parseInt($(".inactiveCount").text()) - 1;
+
+            $(".flagCount").text(flagCount);
+            $(".inactiveCount").text(inactiveCount);
+
+
+        } else {
+            $(ui.item).find('.removed-by').empty();
+            context = " no longer flagged for removal."
+            action = 0;
+
+            var flagCount = parseInt($(".flagCount").text()) - 1,
+                inactiveCount = parseInt($(".inactiveCount").text()) + 1;
+
+            $(".flagCount").text(flagCount);
+            $(".inactiveCount").text(inactiveCount);
+
+        }
+
+        $.ajax({
+            type: 'POST',
+            url: '/application/controllers/update_flagged.php',
+            data: {
+                action: action,
+                id: itemMoved
+            },
+            dataType: 'json',
+            success: function(response) {
+
+                if (response.success === false) {
+
+                    message = response.message;
+                    $(".alert-box").stop().html("<div class='alert alert-danger'><i class='fa fa-times'></i> " + message + "</div>").effect('highlight').delay(1000).fadeOut();
+                } else {
+
+                    message = "Player " + itemMoved + context;
+                    $(".alert-box").stop().html("<div class='alert alert-success'><i class='fa fa-check'></i> " + message + "</div>").effect('highlight').delay(1000).fadeOut();
+                }
+
+
+
+            },
+
+            // fail: function()
+        });
+
+    }
+});
+
+$(".draggable").draggable({
+    connectToSortable: 'ul'
+});
+
+var itemMoved, targetplatoon, sourcePlatoon;
+$(".sortable").sortable({
+    connectWith: 'ul',
+    placeholder: "ui-state-highlight",
+    receive: function(event, ui) {
+        itemMoved = $(ui.item).attr('data-id');
+        targetPlatoon = $(this).attr('id');
+        alert("Player " + itemMoved + " now belongs to " + targetPlatoon);
+
+    }
+});
+
+$(function() {
+
+    $(".progress-bar-rct").attr("class", "bar progress-bar progress-bar-striped progress-bar-danger active");
+
+    $('#rootwizard').bootstrapWizard({
+        onNext: function(tab, navigation, index) {
+
+            /**
+             * slide validation
+             */
+
+            if (index == 2) {
+
+                $(".progress-bar").attr("class", "bar progress-bar progress-bar-striped progress-bar-warning active");
+
+                // Validate fields
+                if (!$('#member_id').val() || !$('#battlelog').val() || !$('#forumname').val()) {
+                    $(".message").html("<i class='fa fa-times'></i>  All fields are required.").effect("bounce");
+                    $('[class$=group]').each(function() {
+                        var $this = $(this);
+                        if ($this.find('input').val() == '') {
+                            $(this).addClass("has-error");
+                        }
+                    });
+
+                    return false;
+                }
+
+                // grab values since we know they exist
+                var forumName = $('#forumname').val(),
+                    battlelog = $('#battlelog').val(),
+                    platoon = $('#platoon').val(),
+                    squadLdr = $('#squadLdr').val(),
+                    member_id = $('#member_id').val();
+
+                if (/\D/.test(member_id)) {
+                    $(".message").html("<i class='fa fa-times'></i> Member id must be a number.").effect("bounce");
+                    return false;
+                }
+
+                // no errors, so clear any error states
+                $(".has-error").removeClass("has-error");
+                $(".message").html("");
+
+
+                // check for matching forum name / battlelog
+                if (battlelog != forumName) {
+                    if (!confirm("The member's forum name does not match the ingame name. Are you sure you wish to continue with this information?")) {
+                        return false;
+                    }
+                }
+
+                // post member data to db
+                var flag = 0;
+                alert('Fetching BF4DB ID. Browser may freeze momentarily...');
+
+                $.ajax({
+                    type: 'POST',
+                    url: '/application/controllers/store_member.php',
+                    data: {
+                        name: forumName,
+                        battlelog: battlelog,
+                        member_id: member_id,
+                        platoon: platoon,
+                        squadLdr: squadLdr
+                    },
+                    dataType: 'json',
+                    async: false,
+                    success: function(response) {
+                        if (response.success === false) {
+                            flag = 0;
+                            message = response.message;
+                            if (response.battlelog === true) {
+                                $(".battlelog-group").addClass('has-error');
+                            } else if (response.memberExists === true) {
+                                $(".memberid-group").addClass('has-error');
+                            }
+                        } else {
+                            flag = 1;
+                        }
+                    }
+                });
+
+                // have to declare a flag so it's not undefined...
+                if (flag == 0) {
+                    $(".message").html("<i class='fa fa-times'></i> " + message).effect('bounce');
+                    return false;
+                } else {
+                    $(".alert-box").append("<div class='alert alert-success' style='z-index: 5;'><i class='fa fa-check fa-2x'></i> Your new recruit has been added to the database!</div>").delay(2000).fadeOut();
+                    return true;
+                }
+
+                $(".progress-bar").attr("class", "bar progress-bar progress-bar-striped progress-bar-warning active");
+            }
+
+
+            if (index == 3) {
+
+                // have the division threads loaded?
+                if ($('.thread-list').is(':visible')) {
+
+                    // do the number of threads match the number of successful results?
+                    if ($('li.thread').length != $('.thread span.alert-success').length) {
+                        $(".thread-status").html(" <i class='fa fa-times'></i> Recruit must complete all threads. ").effect('highlight')
+                        return false
+                    }
+                } else {
+                    return false
+                }
+            }
+
+            if (index == 4) {
+                $(".progress-bar").attr("class", "bar progress-bar progress-bar-striped progress-bar-success active");
+            }
+
+        },
+        onTabShow: function(tab, navigation, index) {
+
+            // panel titles
+            switch (index) {
+                case 0:
+                    $(".tab-title strong").html("Recruiting Introduction")
+                    break;
+                case 1:
+                    $(".tab-title strong").html("Add new member information")
+                    break;
+                case 2:
+                    $(".tab-title strong").html("Rules and Regulations Threads")
+                    loadThreadCheck();
+                    break;
+                case 3:
+                    $(".tab-title strong").html("Finishing Up With Your Recruit")
+                    break;
+                case 4:
+                    $(".tab-title strong").html("\"Dreaded Paperwork\"")
+                    break;
+                case 5:
+                    $(".tab-title strong").html("Recruitment Complete")
+                    break;
+            }
+
+            var $total = navigation.find('li').length;
+            var $current = index + 1;
+            var $percent = ($current / $total) * 100;
+            $('#rootwizard').find('.bar').css({
+                width: $percent + '%'
+            });
+        }
+    });
+});
+
+
+function loadThreadCheck() {
+
+    console.log("Thread check init.");
+
+    // setting these here since we know we have them
+    var player = $('#forumname').val(),
+        battlelog = $('#battlelog').val(),
+        game = $("#game").val(),
+        member_id = $("#member_id").val(),
+
+        // division structure
+        postCode = "Please add:<br />Full-time<br />AOD_Rct_" + ucwords(player) + "<br />http://www.clanaod.net/forums/member.php?u=" + member_id + "<br />http://battlelog.battlefield.com/bf4/user/" + battlelog,
+        postCopy = "Please add:\r\nFull-time\r\nAOD_Rct_" + ucwords(player) + "\r\nhttp://www.clanaod.net/forums/member.php?u=" + member_id + "\r\nhttp://battlelog.battlefield.com/bf4/user/" + battlelog,
+
+        // welcome PM -- needs to come from the DB
+        welcomeCode = "[b]Congratulations " + ucwords(player) + ", you've been accepted to join AOD![/b] I hope your stay so far has been positive.<br /><br />Here's a couple of key points to get you started and keep you going:<br /><br />[list][*]**Most Important** You'll need to be on Teamspeak any time you are in game. You dont have to be talking to others (we have a quiet room) but we use vent to communicate and organize ourselves.[*]AOD has a military structure. We do not enforce it oppressively, but we do require a minimum level of respect. Check it out here.http://www.clanaod.net/forums/showthread.php?t=3326[*]AOD has a Code of Conduct. The summary is, be respectful of others, they will already be respecting you, otherwise they wouldnt be here. http://www.clanaod.net/forums/showthread.php?t=3327[*]AOD has several games. If you play any of the games AOD supports, make sure you throw on the AOD tags.[*]*** We check the forums periodically for member activity to make sure we aren't carrying dead weight. AOD is a huge clan, and it's important that we clean house periodically. If you feel that you will be absent for any reason, you can request an LOA and your membership status will be maintained. There is LOTS of cool stuff, events, and announcements in the Battlefield 4 forums. ***[*]We like to screenshot when we take each other's tags. We post the screenshots here on our wall of shame. I hope very much to personally add your tags to the wall! =) ... The wall is HERE: http://www.clanaod.net/forums/showthread.php?t=75595[/list]<br /><br />IMPORTANT:<br />Also remember, your forum username will change in a day or so to AOD_" + player + ", so don't panic! It is our way of keeping track of who has been processed in properly, and given access to the member only forums. You will NOT receive an email reminding you of this, however if you need to do a password recovery, it will remind you that your username is not exactly what you registered with.<br /><br />Most importantly, HAVE FUN, and PLAY TOGETHER. Feel free to PM Me, BL Chat me, or pull me aside in Teamspeak if you have ANY questions at all. You can also feel free to contact any other NCO with questions.",
+
+        welcomeCopy = "[b]Congratulations " + ucwords(player) + ", you've been accepted to join AOD![/b] I hope your stay so far has been positive.\r\n\r\nHere's a couple of key points to get you started and keep you going:\r\n\r\n[list][*]**Most Important** You'll need to be on Teamspeak any time you are in game. You dont have to be talking to others (we have a quiet room) but we use vent to communicate and organize ourselves.[*]AOD has a military structure. We do not enforce it oppressively, but we do require a minimum level of respect. Check it out here.http://www.clanaod.net/forums/showthread.php?t=3326[*]AOD has a Code of Conduct. The summary is, be respectful of others, they will already be respecting you, otherwise they wouldnt be here. http://www.clanaod.net/forums/showthread.php?t=3327[*]AOD has several games. If you play any of the games AOD supports, make sure you throw on the AOD tags.[*]*** We check the forums periodically for member activity to make sure we aren't carrying dead weight. AOD is a huge clan, and it's important that we clean house periodically. If you feel that you will be absent for any reason, you can request an LOA and your membership status will be maintained. There is LOTS of cool stuff, events, and announcements in the Battlefield 4 forums. ***[*]We like to screenshot when we take each other's tags. We post the screenshots here on our wall of shame. I hope very much to personally add your tags to the wall! =) ... The wall is HERE: http://www.clanaod.net/forums/showthread.php?t=75595[/list]\r\n\r\nIMPORTANT:\r\nAlso remember, your forum username will change in a day or so to AOD_" + player + ", so don't panic! It is our way of keeping track of who has been processed in properly, and given access to the member only forums. You will NOT receive an email reminding you of this, however if you need to do a password recovery, it will remind you that your username is not exactly what you registered with.\r\n\r\nMost importantly, HAVE FUN, and PLAY TOGETHER. Feel free to PM Me, BL Chat me, or pull me aside in Teamspeak if you have ANY questions at all. You can also feel free to contact any other NCO with questions.";
+
+
+    if (battlelog) {
+        $(".rank-name").html("AOD_Rct_" + ucwords(battlelog));
+        $(".player-name").html(ucwords(battlelog));
+
+        // full name copy
+        $('.player-name-copy').attr("data-clipboard-text", "AOD_Rct_" + ucwords(battlelog))
+
+        // division structure
+        $("#division-post .post-code").html(postCode);
+        $('.division-code-btn').attr("data-clipboard-text", postCopy);
+
+        // final member id for request
+        $(".final_member_id").html(member_id);
+        $('.member-status-btn').attr("data-clipboard-text", member_id);
+
+        // welcome PM
+        $("#welcome-pm .welcome-code").html(welcomeCode);
+        $('.welcome-pm-btn').attr("data-clipboard-text", welcomeCopy);
+        $(".pm-link").click(function(e) {
+            e.preventDefault();
+            windowOpener($(this).attr("href") + member_id, "AOD Squad Tracking", "width=1000,height=600,scrollbars=yes");
+        });
+
+    }
+
+    if (player) {
+        $(".search-subject").html("<p class='text-muted'>Searching threads for posts by: <code>" + ucwords(player) + "</code></p>");
+    }
+
+    $(".thread-results").html('<img src="/public/images/loading.gif " class="margin-top-20" />');
+
+    $.ajax({
+        url: "/application/controllers/recruit_thread_check.php",
+        data: {
+            player: player,
+            game: game
+        },
+        cache: false,
+        beforeSend: function() {
+            $('#content').hide();
+            $('#loading').show();
+        },
+    })
+
+    .done(function(html) {
+        $(".thread-results ").empty().prepend(html);
+
+        $('.tool').powerTip({
+            placement: 'n'
+        });
+
+
+    });
+}
+
+
+
+$(function() {
+
+    // auto select values
+    var sqdldr = $("#cur_sqd").val(),
+        plt = $("#cur_plt").val(),
+        pos = $("#cur_pos").val();
+
+    $("#platoon option[value=" + plt + "]").attr("selected", "selected");
+    $("#sqdldr option[value=" + sqdldr + "]").attr("selected", "selected");
+    $("#position option[value=" + pos + "]").attr("selected", "selected");
+
+    $("#edit-form").submit(function(event) {
+        event.preventDefault();
+
+        $("#edit-form :submit").html("Loading").attr('class', 'btn btn-block btn-default disabled');
+
+        var uid = $("#uid").val(),
+            mid = $("#member_id").val(),
+            fname = $("#forum_name").val(),
+            platoon = $("#platoon").val(),
+            sqdldr = $("#sqdldr").val(),
+            blog = $("#battlelog").val(),
+            position = $("#position").val();
+
+        updateMember(uid, mid, fname, blog, platoon, sqdldr, position);
+    });
+
+});
+
+function updateMember(uid, mid, fname, blog, platoon, sqdldr, position) {
+    setTimeout(function() {
+        $.post("/application/controllers/update_member.php", {
+                uid: uid,
+                mid: mid,
+                fname: fname,
+                blog: blog,
+                platoon: platoon,
+                squad: sqdldr,
+                position: position
+            },
+
+            function(data) {
+                $("#edit-form :submit").html("Submit Info").attr('class', 'btn btn-block btn-success');
+                if (data.success === false) {
+                    if (data.battlelog === false) {
+                        $("#edit-form .battlelog-group").addClass("has-error");
+                    }
+                    $("#edit-form .message").html(data.message).addClass("alert-danger").show();
+
+                    return false;
+                } else {
+                    $("#edit-form .message").show().html(data.message).removeClass("alert-danger").addClass('alert-success').delay(1000).fadeOut();
+                    $(".has-error").removeClass("has-error");
+                }
+
+            }, "json")
+    }, 1000)
+}
+
+
+
+function ucwords(str) {
+    return (str + '')
+        .replace(/^([a-z\u00E0-\u00FC])|\s+([a-z\u00E0-\u00FC])/g, function($1) {
+            return $1.toUpperCase();
+        });
+}
+
+
+function windowOpener(url, name, args) {
+
+    if(typeof(popupWin) != "object" || popupWin.closed)  { 
+        popupWin =  window.open(url, name, args); 
+    } 
+    else{ 
+        popupWin.location.href = url; 
+    }
+
+    popupWin.focus(); 
+ }
