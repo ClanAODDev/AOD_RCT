16fe10724b8eea573eb6c2156ce3fae8ca01a3b7
Chris Deaton <dcdeaton@ncsu.edu>
2015-03-01 11:54:04 -0500

    add cancel button, not functional

--- application/views/recruiting/new_member.php
+++ application/views/recruiting/new_member.php
@@ -378,7 +378,9 @@ $out .= "
 					<li class='previous first' style='display:none;'><a href='#'>First</a></li>
 					<li class='previous'><a href='#'>Previous</a></li>
 					<li class='next last' style='display:none;'><a href='#'>Last</a></li>
+					
 					<li class='next'><a href='#'>Continue</a></li>
+					<li class='cancel-rct'><a href='#'>Cancel Recruiting Process</a></li>
 				</ul>
 			</div>
 
